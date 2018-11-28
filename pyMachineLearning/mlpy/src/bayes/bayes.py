537a181c71ffc206f3f67d812837a1d3f660d832
kokomal <mattleblanc@live.cn>
2018-11-28 17:04:29 +0800

    bayes垃圾邮件整理

--- pyMachineLearning/mlpy/src/bayes/bayes.py
+++ pyMachineLearning/mlpy/src/bayes/bayes.py
@@ -14,7 +14,7 @@ def loadDataSet():
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'how', 'him'],  # 注意这里的2个how
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
     return postingList, classVec
@@ -34,6 +34,67 @@ def setOfWords2Vec(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else: print("the word: \"%s\" is not in my Vocabulary!" % word)
     return returnVec
+
+
+# 训练贝叶斯模型，原始版本
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)  # 多少个样本
+    numWords = len(trainMatrix[0])  # 词袋的维度
+    pAbusive = sum(trainCategory) / float(numTrainDocs)  # P(Ci)
+    p0Num = ones(numWords); p1Num = ones(numWords)  # [1,1,1,...,1,1]，避免含0的情况
+    p0Denom = 2.0; p1Denom = 2.0  # 防止除0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:  # trainMatrix[n] = [1,0,1,...,1]
+            p1Num += trainMatrix[i]  # 叠加上去[a0,1+n0, ... 1+m0,...]
+            p1Denom += sum(trainMatrix[i])  # 归一化的分母
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = (p1Num / p1Denom)
+    p0Vect = (p0Num / p0Denom)
+    print("p0Denom=%d, p1Denom=%d" % (p0Denom, p1Denom))
+    return p0Vect, p1Vect, pAbusive
+
+
+# 分类，P(Ci|w) = P(w|Ci)*P(Ci) / P(w)
+# P(w)略掉，P(Ci)在本例中是PAb=0.5
+# P(w|Ci) = ∏ P(w|Ci) for each i
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # element-wise mult
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    print("P1=%f, while P0=%f" % (p1, p0))
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+# 训练贝叶斯模型LOG
+def trainNB0Log(trainMatrix, trainCategory):
+    p0V, p1V, pAbusive = trainNB0(trainMatrix, trainCategory)
+    p1Vect = log(p1V)  # change to np.log()，改为log是为了防止多个小数相乘之后下溢
+    p0Vect = log(p0V)  # change to np.log()
+    return p0Vect, p1Vect, pAbusive
+
+
+# 改进的词袋模型，不再是[0,1,1,0]之类，而是带有频次[0,3,2,1]
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
+
+# 分词工具
+def textParse(bigString):  # 输入为巨型文本，输出为词数组，单个滤掉
+    import re
+    listOfTokens = re.split(r'\W+', bigString)  # 正则，忽略标点符号和空格
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]  # 头部小写，长度大于2
+
+
+# 垃圾邮件测试
+
 
 
 if __name__ == '__main__':
--- pyMachineLearning/mlpy/src/test/bayes/testBayes.py
+++ pyMachineLearning/mlpy/src/test/bayes/testBayes.py
@@ -12,6 +12,7 @@ from numpy import *
 import os
 import matplotlib
 import matplotlib.pyplot as plt
+import io
 
 
 # 测试朴素贝叶斯算法
@@ -55,6 +56,99 @@ class TestBayes(unittest.TestCase):  # 继承unittest.TestCase
             print("RAW WORD IS %s" % wd)
             print("OUTPUT VEC IS %s" % bayes.setOfWords2Vec(vocabList, wd)) 
 
+    def test_word2vecBag(self):
+        raw = bayes.loadDataSet()[0]
+        vocabList = bayes.createVocabList(raw)
+        print("THE VOCABLIST IS %s" % vocabList)
+        for wd in raw:
+            print("RAW WORD IS %s" % wd)
+            print("OUTPUT VEC BAG IS %s" % bayes.bagOfWords2VecMN(vocabList, wd))  # 注意输出的向量里面how会有2个
 
+    def test_trainNB(self):
+        listOPosts, listClasses = bayes.loadDataSet()
+        myVocabList = bayes.createVocabList(listOPosts)
+        print(myVocabList)  # 获得所有词集合，去重
+        trainMat = []
+        for postinDoc in listOPosts:
+            trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
+        print(trainMat)
+        p0V, p1V, pAb = bayes.trainNB0(array(trainMat), array(listClasses))
+        print(p0V)
+        print(p1V)
+        print(pAb)
+        sm1 = sum(p0V)
+        sm2 = sum(p1V)
+        print("sm1=%f, sm2=%f" % (sm1, sm2))  # 和不一定为1，因为做了防除0的改造
+        print("USING LOG DISP")
+        p0V, p1V, pAb = bayes.trainNB0Log(array(trainMat), array(listClasses))
+        print(p0V)
+        print(p1V)
+        print(pAb)
+
+    def test_NB(self):
+        listOPosts, listClasses = bayes.loadDataSet()
+        myVocabList = bayes.createVocabList(listOPosts)
+        trainMat = []
+        for postinDoc in listOPosts:
+            trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
+        p0V, p1V, pAb = bayes.trainNB0Log(array(trainMat), array(listClasses))
+        testEntry = ['love', 'my', 'dalmation']
+        thisDoc = array(bayes.setOfWords2Vec(myVocabList, testEntry))
+        print(thisDoc)
+        print(testEntry, 'classified as: ', bayes.classifyNB(thisDoc, p0V, p1V, pAb))
+        testEntry = ['stupid', 'garbage']
+        thisDoc = array(bayes.setOfWords2Vec(myVocabList, testEntry))
+        print(thisDoc)
+        print(testEntry, 'classified as: ', bayes.classifyNB(thisDoc, p0V, p1V, pAb))
+
+    def test_simpleTextParse(self):  # 测试分词，不去重
+        raw = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis velit tempus, ultricies lectus non, suscipit diam. Pellentesque nunc felis, pellentesque a finibus non, mattis at urna. Cras erat mi, tincidunt at sodales nec, consectetur eu turpis. Aliquam ornare auctor massa sed posuere. Praesent lobortis lorem in feugiat lobortis. Pellentesque dapibus semper tristique. Aenean accumsan odio eget est efficitur, at lobortis nibh hendrerit.Nunc non lacus magna. Duis convallis est quam, vel consequat neque ultricies vel. Mauris fringilla malesuada orci. Curabitur et eros finibus, auctor quam at, dignissim nisi. Phasellus eu ligula feugiat, blandit mi et, tempus odio. Aliquam fringilla sed velit eget finibus. Mauris semper est nulla, id convallis quam sodales a. Donec mollis erat eget lacus consequat, quis aliquam diam porttitor. Etiam fringilla tempus lacus at posuere. In hac habitasse platea dictumst. Etiam eu ligula congue, tempor lacus ac, volutpat nisl. Etiam consectetur tortor in ipsum tempor rhoncus. Nam blandit gravida faucibus. Cras in commodo nibh. Duis lacinia bibendum sodales.'
+        print(bayes.textParse(raw))
+        raw = io.open('email/ham/%d.txt' % 6, encoding="ISO-8859-1").read()
+        print(raw)
+        print(bayes.textParse(raw))
+    
+    def spamTest(self):
+        docList = []; classList = []; fullText = []
+        for i in range(1, 26):
+            wordList = bayes.textParse(io.open('email/spam/%d.txt' % i, encoding="ISO-8859-1").read())
+            docList.append(wordList)
+            fullText.extend(wordList)
+            classList.append(1)
+            wordList = bayes.textParse(io.open('email/ham/%d.txt' % i, encoding="ISO-8859-1").read())
+            docList.append(wordList)
+            fullText.extend(wordList)
+            classList.append(0)
+        vocabList = bayes.createVocabList(docList)  # create vocabulary
+        print("VACABULIST IS %s \nwith VACABULIST size = %d" % (vocabList, len(vocabList)))
+        trainingSet = range(50); testSet = []  # create test set
+        for i in range(10):
+            randIndex = int(random.uniform(0, len(trainingSet)))
+            print("RANDOM IS %d" % randIndex)
+            testSet.append(trainingSet[randIndex])
+            del(trainingSet[randIndex]) # 2和3的语法不一样
+        trainMat = []; trainClasses = []
+        for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+            trainMat.append(bayes.bagOfWords2VecMN(vocabList, docList[docIndex]))
+            trainClasses.append(classList[docIndex])
+        print(trainMat)
+        p0V, p1V, pSpam = bayes.trainNB0(array(trainMat), array(trainClasses)) # 得到训练后的朴素贝叶斯向量P(w|Ci)和P(Ci)
+        errorCount = 0
+        for docIndex in testSet:  # classify the remaining items
+            wordVector = bayes.bagOfWords2VecMN(vocabList, docList[docIndex])
+            if bayes.classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+                errorCount += 1
+                print("classification error", docList[docIndex])
+        print('the error rate is: ', float(errorCount) / len(testSet))
+        # return vocabList, fullText
+
+    def test_rangeDel(self):
+        xx = range(10)
+        del(list(xx)[2])
+        print(xx)
+        del(xx[2]) 
+        print(xx)
+        print (sys.version)
+         
 if __name__ == '__main__':
     unittest.main()  # 运行所有的测试用例
