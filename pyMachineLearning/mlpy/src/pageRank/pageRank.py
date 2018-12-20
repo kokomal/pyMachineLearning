f4b34bb6724202308fafe22a2dc5d9adc5980bab
kokomal <mattleblanc@live.cn>
2018-12-20 18:11:16 +0800

    Louvain细化

--- pyMachineLearning/mlpy/src/pageRank/pageRank.py
+++ pyMachineLearning/mlpy/src/pageRank/pageRank.py
@@ -6,6 +6,8 @@
 # Return v, a vector of ranks such that v_i is the i-th rank from [0, 1]
 
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 # 唯一输入 相邻矩阵
@@ -20,7 +22,8 @@ def pagerank(M, eps=1.0e-8, d=0.85):
     # print(v)
     last_v = np.ones((N, 1), dtype=np.float32) * 100
     M_hat = (d * M) + (((1 - d) / N) * np.ones((N, N), dtype=np.float32))  # 一次性处理相邻矩阵M即可
-    # print(M_hat)
+    print("M-Hat")
+    print(M_hat)
     iter = 1
     while np.linalg.norm(v - last_v, 2) > eps:
         iter += 1
@@ -30,12 +33,105 @@ def pagerank(M, eps=1.0e-8, d=0.85):
     return v
 
 
-M = np.array([[0, 0, 0, 0, 1],
-              [0.45, 0, 0, 0, 0],
-              [0.55, 0, 0, 0, 0],
-              [0, 1, 0.5, 0, 0],
-              [0, 0, 0.5, 1, 0]])
-v = pagerank(M, 0.001, 0.31) # 生物学采用0.31
-print(v)
-v = pagerank(M, 0.001) # 搜索引擎采用0.85
-print(v)
+if __name__ == "__main__":
+    # 第一步对M的全0列进行修正，可以是全平摊，也可以是全孤岛
+    M = np.array([[0, 0, 0, 0, 1],
+                  [0.4277, 1, 0, 0, 0],
+                  [0.5723, 0, 0, 0, 0],
+                  [0, 0, 0.5, 0, 0],
+                  [0, 0, 0.5, 1, 0]])
+    v = pagerank(M, 0.001, 0.31)  # 生物学采用0.31
+    print(v)
+    v = pagerank(M, 0.001)  # 搜索引擎采用0.85,对于孤岛节点，0.85的惩罚系数有点狠了
+    print(v)
+
+
+# 优化版PageRank算法
+class PageRank():
+
+    @classmethod
+    def fetchFromCSV(cls, path):
+        f = open(path, 'r')
+        lines = f.readlines()
+        f.close()
+        nodeset = set() # 节点采用set
+        edges = {}
+        for line in lines:
+            n = line.split(',')
+            if not n:
+                break
+            #nodes[n[0]] = 1
+            #nodes[n[2]] = 1
+            nodeset.add(n[0])
+            nodeset.add(n[2])
+            w = 1
+            if len(n) == 4:
+                w = int(n[3])
+            if n[1] == 'in': # caller还是callee
+                key = (n[0], n[2])
+            else:
+                key = (n[2], n[0])
+            if key in edges:
+                # 这里可以施加数据清洗的操作，例如多次通话的频次、时长的统计学计算等，目前这里仅仅对通话时间进行叠加
+                edges[key] += w
+            else:
+                edges[key] = w
+        return cls(nodeset, edges)
+        
+    def __init__(self, nodeset, edges):
+        self.nodeset = nodeset
+        self.edges = edges
+        self.sorted_node_index_map = {}
+        self.sorted_nodes = sorted(self.nodeset)
+        i = 0
+        for nd in self.sorted_nodes:
+            self.sorted_node_index_map[nd] = i
+            i += 1
+        print(self.sorted_node_index_map)
+        # sorted_node_index_map用来给出排序好的{用户:序列}
+
+    def genAdjacentMatrix(self):
+        N = len(self.sorted_nodes)
+        rawMat = np.eye(N) # 防止全0列
+        for edgeTup in self.edges.keys():
+            frm = edgeTup[0]
+            to = edgeTup[1]
+            wt = self.edges[edgeTup]
+            frmIdx = self.sorted_node_index_map[frm]
+            toIdx = self.sorted_node_index_map[to]
+            print('edgeTup', edgeTup)
+            print('wt', wt)
+            rawMat[toIdx][toIdx] = 0
+            rawMat[frmIdx][toIdx] = wt
+        print(rawMat) 
+        norm = np.linalg.norm(rawMat, axis=0, ord=1)  # 标准化，求得各个列的和
+        # print(norm)
+        rawMat = rawMat / norm
+        return rawMat
+
+
+    # 简易绘制交互图
+    def draw(self, picName):
+        G = nx.DiGraph()
+        for k in self.edges.keys():
+            G.add_edge(k[0], k[1], weight=self.edges[k])
+        # nx.draw(G, node_size=300, with_labels=True)
+        # nx.draw(G, pos=nx.spring_layout(G), node_color='r', edge_color='g', with_labels=True, font_size=18, node_size=300)
+        pos = nx.spring_layout(G)  # positions for all nodes
+        nx.draw_networkx_nodes(G, pos, node_size=500)
+        # 根据权重，实线为权值大的边，虚线为权值小的边
+        # edges
+        elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 10]
+        esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= 10]
+        # labels标签定义
+        edge_labels = dict([((u, v,), d['weight']) for u, v, d in G.edges(data=True)])
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        
+        nx.draw_networkx_labels(G, pos, font_size=8, font_family='sans-serif')
+        nx.draw_networkx_edges(G, pos, edgelist=elarge,
+                            width=4, edge_color='r', arrows=True, arrowsize=6)
+        nx.draw_networkx_edges(G, pos, edgelist=esmall,
+                            edge_color='b', style='dashed')
+        plt.axis('off')
+        plt.savefig(picName)
+        plt.show()
--- pyMachineLearning/mlpy/src/test/pageRank/testPageRank.py
+++ pyMachineLearning/mlpy/src/test/pageRank/testPageRank.py
@@ -13,6 +13,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import networkx as nx  # 导入networkx包
+from pageRank.pageRank import PageRank
 
 
 # 测试逻辑回归
@@ -72,7 +73,7 @@ class TestPageRank(unittest.TestCase):  # 继承unittest.TestCase
         G.add_node(2)
         G.add_nodes_from([3, 4, 5, 6])
         G.add_cycle([1, 2, 3, 4])
-        G.add_edge(1, 3)
+        G.add_edge(1, 3, weight=10)
         G.add_edges_from([(3, 5), (3, 6), (6, 7)])
         nx.draw(G, node_size=300, with_labels=True)
         plt.savefig("youxiangtu.png")
@@ -84,6 +85,119 @@ class TestPageRank(unittest.TestCase):  # 继承unittest.TestCase
         plt.savefig("ba.png")  # 输出方式1: 将图像存为一个png格式的图片文件
         plt.show()  # 输出方式2: 在窗口中显示这幅图像
 
+    def test_draw3(self):
+        __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
+        try:
+            import matplotlib.pyplot as plt
+        except:
+            raise
+        import networkx as nx
+         
+        G = nx.Graph()
+        # 添加带权边
+        G.add_edge('a', 'b', weight=0.6)
+        G.add_edge('a', 'c', weight=0.2)
+        G.add_edge('c', 'd', weight=0.1)
+        G.add_edge('c', 'e', weight=0.7)
+        G.add_edge('c', 'f', weight=0.9)
+        G.add_edge('a', 'd', weight=0.3)
+        # 按权重划分为重权值得边和轻权值的边
+        elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 0.5]
+        esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= 0.5]
+        # 节点位置
+        pos = nx.shell_layout(G)  # positions for all nodes
+        # 首先画出节点位置
+        # nodes
+        nx.draw_networkx_nodes(G, pos, node_size=700)
+        # 根据权重，实线为权值大的边，虚线为权值小的边
+        # edges
+        nx.draw_networkx_edges(G, pos, edgelist=elarge,
+                            width=10)
+        nx.draw_networkx_edges(G, pos, edgelist=esmall,
+                            width=2, alpha=0.5, edge_color='b', style='dashed')
+         
+        # labels标签定义
+        nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+         
+        plt.axis('off')
+        plt.savefig("weighted_graph.png")  # save as png
+        plt.show()  # display
+
+    def test_draw4(self):
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        
+        G = nx.DiGraph()
+        G.add_edges_from(
+            [('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),
+             ('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')])
+        
+        val_map = {'A': 1.0,
+                   'D': 0.5714285714285714,
+                   'H': 0.0}
+        
+        values = [val_map.get(node, 0.25) for node in G.nodes()]
+        
+        # Specify the edges you want here
+        red_edges = [('A', 'C'), ('E', 'C')]
+        edge_colours = ['black' if not edge in red_edges else 'red'
+                        for edge in G.edges()]
+        black_edges = [edge for edge in G.edges() if edge not in red_edges]
+        
+        # Need to create a layout when doing
+        # separate calls to draw nodes and edges
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+                               node_color=values, node_size=500)
+        nx.draw_networkx_labels(G, pos)
+        nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
+        nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=False)
+        plt.show()
+
+    def test_draw5(self):
+        import networkx as nx
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import pylab
+        
+        G = nx.DiGraph()
+        
+        G.add_edges_from([('A', 'B'), ('C', 'D'), ('G', 'D')], weight=1)
+        G.add_edges_from([('D', 'A'), ('D', 'E'), ('B', 'D'), ('D', 'E')], weight=2)
+        G.add_edges_from([('B', 'C'), ('E', 'F')], weight=3)
+        G.add_edges_from([('C', 'F')], weight=4)
+        
+        val_map = {'A': 1.0,
+                           'D': 0.5714285714285714,
+                                      'H': 0.0}
+        
+        values = [val_map.get(node, 0.45) for node in G.nodes()]
+        edge_labels = dict([((u, v,), d['weight'])
+                         for u, v, d in G.edges(data=True)])
+        red_edges = [('C', 'D'), ('D', 'A')]
+        edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
+        
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        nx.draw(G, pos, node_color=values, node_size=1500, edge_color=edge_colors, edge_cmap=plt.cm.Reds)
+        pylab.show()
+
+    def test_pgrk(self):
+        pg = PageRank.fetchFromCSV('test.csv')
+        pg.draw('pic.png')
+    
+    def test_pgrkAlgo(self):
+        pg = PageRank.fetchFromCSV('test.csv')
+        mhat = pg.genAdjacentMatrix()
+        print(mhat)
+        
+    def test_dict(self):
+        dic = {'a':31, 'bc':5, 'c':3, 'asd':4, 'aa':74, 'd':0}
+        sorted_dict= sorted(dic.items(), key=lambda d:d[0]) 
+        print(dic)
+        print(sorted_dict)
+        print(type(dic))
+        print(type(sorted_dict))
 
 if __name__ == '__main__':
     unittest.main()  # 运行所有的测试用例
