4dceaca10aa76b429a157948f9bbd9f8f654a679
kokomal <mattleblanc@live.cn>
2019-02-28 17:39:36 +0800

    pca

--- pyMachineLearning/mlpy/src/pca/pca.py
+++ pyMachineLearning/mlpy/src/pca/pca.py
@@ -2,3 +2,15 @@
 
 from numpy import *
 
+
+def pca(dataMat, topNfeat=9999999):
+    meanVals = dataMat.mean(0)
+    meanRemoved = dataMat - meanVals  # remove mean
+    covMat = cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = linalg.eig(mat(covMat))
+    eigValInd = argsort(eigVals)  # sort, sort goes smallest to largest
+    eigValInd = eigValInd[:-(topNfeat + 1):-1]  # cut off unwanted dimensions
+    redEigVects = eigVects[:, eigValInd]  # reorganize eig vects largest to smallest
+    lowDDataMat = meanRemoved * redEigVects  # transform data into new dimensions
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
--- pyMachineLearning/mlpy/src/test/pca/testPca.py
+++ pyMachineLearning/mlpy/src/test/pca/testPca.py
@@ -11,6 +11,13 @@ from numpy import *
 import os
 import matplotlib
 import matplotlib.pyplot as plt
+from pca import pca
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [list(map(float,line)) for line in stringArr]
+    return mat(datArr)
 
 # 测试SVM算法
 class MyTest(unittest.TestCase):  # 继承unittest.TestCase
@@ -35,3 +42,22 @@ class MyTest(unittest.TestCase):  # 继承unittest.TestCase
         
     def test_hello(self):
         print('hello')
+        
+    def test_pca1(self):
+        dataMat = loadDataSet('testSet.txt')
+        print(dataMat)
+        lowDMat, reconMat = pca.pca(dataMat, 1)
+        print(shape(lowDMat))
+        print(shape(reconMat))
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
+        ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=50, c='red')
+        plt.show()
+        #---------------------------------------------
+        lowDMat, reconMat = pca.pca(dataMat, 2)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
+        ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=50, c='red')
+        plt.show()
